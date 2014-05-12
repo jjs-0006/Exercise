56b17eb15ae3f4f3c926dc88233bac674a73a1e2
jjs-0006 <jjs-0006@outlook.jp>
2014-05-12 16:56:12 +0900

    q1_11

--- src/jp/co/jjs/java_seminar/self_study/q1_10/Exercise2.java
+++ src/jp/co/jjs/java_seminar/self_study/q1_10/Exercise2.java
@@ -2,7 +2,7 @@ package jp.co.jjs.java_seminar.self_study.q1_10;
 
 public class Exercise2 {
     public static void main(String[] args) {
-        String myName = "芳賀" + "雄基";
+        String myName = "山田" + "太郎";
         System.out.println(myName);
     }
 }
---
+++ src/jp/co/jjs/java_seminar/self_study/q1_11/Exercise2.java
@@ -0,0 +1,10 @@
+package jp.co.jjs.java_seminar.self_study.q1_11;
+
+public class Exercise2 {
+
+    public static void main(String[] args) {
+        System.out.println("New York,Los Angels間を75マイル/hで運転した場合");
+        System.out.println(3000/75 + "時間かかる");
+    }
+}
+
