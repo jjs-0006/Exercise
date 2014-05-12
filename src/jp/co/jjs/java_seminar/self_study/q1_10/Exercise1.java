52f39c268fc350852a2425473a1d139bd0cae0ed
jjs-0006 <jjs-0006@outlook.jp>
2014-05-12 16:41:39 +0900

    q1_9,q1_10

---
+++ src/jp/co/jjs/java_seminar/self_study/q1_10/Exercise1.java
@@ -0,0 +1,10 @@
+package jp.co.jjs.java_seminar.self_study.q1_10;
+
+public class Exercise1 {
+
+    public static void main(String[] args) {
+        System.out.println("\"ダブルクォーテーション\"");
+
+    }
+
+}
---
+++ src/jp/co/jjs/java_seminar/self_study/q1_10/Exercise2.java
@@ -0,0 +1,8 @@
+package jp.co.jjs.java_seminar.self_study.q1_10;
+
+public class Exercise2 {
+    public static void main(String[] args) {
+        String myName = "芳賀" + "雄基";
+        System.out.println(myName);
+    }
+}
