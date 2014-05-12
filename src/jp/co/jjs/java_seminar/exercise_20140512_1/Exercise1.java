ad2f4e58df07ae050ff7540b1e6935bc9902a9ec
jjs-0006 <jjs-0006@outlook.jp>
2014-05-12 15:32:32 +0900

    exercise1

---
+++ src/jp/co/jjs/java_seminar/exercise_20140512_1/Exercise1.java
@@ -0,0 +1,22 @@
+package jp.co.jjs.java_seminar.exercise_20140512_1;
+
+public class Exercise1 {
+    public static void main(String[] args) {
+        boolean bo = true;
+        byte by = 20;
+        short s = 347;
+        int i = 2014;
+        long l = 2038842;
+        float f = (float) 1.3232;
+        double d = 3.48928492;
+        char c = 'a';
+        System.out.println("boolean:" + bo);
+        System.out.println("byte:" + by);
+        System.out.println("short:" + s);
+        System.out.println("int:" + i);
+        System.out.println("long:" + l);
+        System.out.println("float:" + f);
+        System.out.println("double:" + d);
+        System.out.println("char:" + c);
+    }
+}
