3e3cf9ef795188d2994beb8c07463c5d3d2ba656
jjs-0006 <jjs-0006@outlook.jp>
2014-05-13 13:00:53 +0900

    exercise

--- src/jp/co/jjs/java_seminar/exercise_20140513_4/Exercise6.java
+++ src/jp/co/jjs/java_seminar/exercise_20140513_4/Exercise6.java
@@ -4,7 +4,7 @@ public class Exercise6 {
 
     public static void main(String[] args) {
         int n = 2;
-        if(n <= 0){
+        if(n == 0){
             System.out.println("nは0以下です");
         }
         else if(n % 2 == 1){
