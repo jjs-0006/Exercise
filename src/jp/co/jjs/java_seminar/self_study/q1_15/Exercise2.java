a157b5507363d8191e92e0af87be01156c4e56cc
jjs-0006 <jjs-0006@outlook.jp>
2014-05-13 17:28:49 +0900

    exercise

---
+++ src/jp/co/jjs/java_seminar/self_study/q1_15/Exercise2.java
@@ -0,0 +1,11 @@
+package jp.co.jjs.java_seminar.self_study.q1_15;
+
+public class Exercise2 {
+
+    public static void main(String[] args) {
+        byte[] b = new byte[4];
+        System.out.println(b[0] + " " + b[1] + " " + b[2] + " " + b[3]);
+
+    }
+
+}
---
+++ src/jp/co/jjs/java_seminar/self_study/q1_15/Exercise3.java
@@ -0,0 +1,11 @@
+package jp.co.jjs.java_seminar.self_study.q1_15;
+
+public class Exercise3 {
+
+    public static void main(String[] args) {
+        double[] d = {2.5,6.9,0.5,7.5};
+        double avg = (d[0] + d[1] + d[2] + d[3])/4;
+        System.out.println("平均は" + avg);
+    }
+
+}
