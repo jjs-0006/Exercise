8e35ade65446927b7c1272319e915865eaa688b5
jjs-0006 <jjs-0006@outlook.jp>
2014-05-13 12:00:17 +0900

    exercise

---
+++ src/jp/co/jjs/java_seminar/exercise_20140513_4/Exercise6.java
@@ -0,0 +1,18 @@
+package jp.co.jjs.java_seminar.exercise_20140513_4;
+
+public class Exercise6 {
+
+    public static void main(String[] args) {
+        int n = 2;
+        if(n <= 0){
+            System.out.println("nは0以下です");
+        }
+        else if(n % 2 == 1){
+            System.out.println("nは奇数です");
+        }
+        else{
+            System.out.println("nは偶数です");
+        }
+    }
+
+}
