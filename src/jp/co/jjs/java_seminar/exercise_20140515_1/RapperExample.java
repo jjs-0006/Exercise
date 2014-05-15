d072410cd0edfe66c75c4502c24dcdb6625af731
jjs-0006 <jjs-0006@outlook.jp>
2014-05-15 11:27:50 +0900

    ArrayList

---
+++ src/jp/co/jjs/java_seminar/exercise_20140515_1/RapperExample.java
@@ -0,0 +1,15 @@
+package jp.co.jjs.java_seminar.exercise_20140515_1;
+
+public class RapperExample {
+
+    public static void main(String[] args) {
+        Integer i = new Integer(10);
+        System.out.println(i + " " +  i.toString() + " " + i.doubleValue() + " " +i.compareTo(11) + " " + i);
+        Character c = new Character('a');
+        System.out.println(c);
+        Double d = new Double(1.3);
+        System.out.println(Double.MAX_VALUE + " "  + Double.MIN_NORMAL);
+
+    }
+
+}
