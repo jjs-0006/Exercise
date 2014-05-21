570e3a2760d6fd9e35726904ca9b24161905dcae
jjs-0006 <jjs-0006@outlook.jp>
2014-05-21 16:42:11 +0900

    exercise

--- src/jp/co/jjs/java_seminar/exercise_20140519_1/BookReader.java
+++ src/jp/co/jjs/java_seminar/exercise_20140519_1/BookReader.java
@@ -35,7 +35,7 @@ public class BookReader {
         try(BufferedWriter writer = Files.newBufferedWriter(
                 Paths.get(name),
                 Charset.forName("SHIFT-JIS")
-                
+
                 )){
             int i = 0;
             while(i < ar.size()){
@@ -46,7 +46,6 @@ public class BookReader {
             System.out.println("ファイルに出力");
 
         } catch (IOException e) {
-            // TODO 自動生成された catch ブロック
             e.printStackTrace();
         }
     }
